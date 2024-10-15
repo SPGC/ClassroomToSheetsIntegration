36866009ddc9229e2242f5196b3c9c6e9653296f
Ilia Nechaev <lelikk2002@gmail.com>
2024-10-15 17:02:15 +0300

    Fixed bug with multistring private key

--- src/main.rs
+++ src/main.rs
@@ -11,30 +11,24 @@ use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Чтение переменных окружения
     let task_name = env::var("INPUT_TASK_NAME")?;
     let student_github_id = env::var("INPUT_STUDENT_NAME")?;
     let robot_email = env::var("INPUT_ROBOT_EMAIL")?;
-    let private_api_key = env::var("INPUT_PRIVATE_API_KEY")?;
+    let private_api_key_raw = env::var("INPUT_PRIVATE_API_KEY")?;
     let task_result_str = env::var("INPUT_TASK_RESULTS")?;
     let table_id = env::var("INPUT_TABLE_ID")?;
 
-    println!("{}", private_api_key);
+    let private_api_key = private_api_key_raw.replace("\\n", "\n");
 
-    // Преобразование результата задания в число
     let task_result: i32 = task_result_str.parse()?;
 
-    // Получение токена доступа
     let scope = "https://www.googleapis.com/auth/spreadsheets";
     let access_token = get_access_token(&robot_email, &private_api_key, scope).await?;
 
-    // Имя листа (если нужно, можно тоже получить из переменной окружения)
-    let sheet_name = "Sheet1";
+    let sheet_name = env::var("INPUT_SHEET_NAME").unwrap_or("Sheet1".to_string());
 
-    // Создание клиента
     let client = Client::new();
 
-    // Создаем экземпляр StudentManager
     let student_manager = StudentManager::new(
         &client,
         &access_token,
@@ -42,7 +36,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         sheet_name,
     );
 
-    // Обновляем результат задания для студента
     student_manager
         .update_assignment_result(&student_github_id, &task_name, task_result)
         .await?;
